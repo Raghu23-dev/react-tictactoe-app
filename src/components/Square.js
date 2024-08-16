f876fea99760d1f37b15ea80ecf8471aded812c0
raghu23-dev <raghu2308.dev@gmail.com>
2024-08-16 11:13:52 +0530

    passing properties to squares

---
+++ src/components/Square.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Square({value}) {
+  return (
+    <>
+    <div className="square">
+      <button className="square">{value}</button>
+     
+    </div>
+  
+  </>  )
+}
